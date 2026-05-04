5b0fa2b161c532e8479b7ddefe7f110b142ed66a
MarcosPaulo-Ribeiro <marcos.ribeiro@sptech.school>
2026-05-04 14:39:05 -0300

    trocando o IP por Localhost

--- sensor-temperatura/grafico/main.js
+++ sensor-temperatura/grafico/main.js
@@ -19,7 +19,7 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: '10.18.32.72',
+            host: 'localhost',
             user: 'usuarioInsert',
             password: 'Winesense#2026',
             database: 'wineSense',
