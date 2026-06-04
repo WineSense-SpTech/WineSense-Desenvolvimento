2e53ef5a9c0ebd8e5f77d45bfc2d9a43ca0c2dfc
martelozzo <enzo.martelozzo@sptech.school>
2026-06-03 23:37:22 -0300

    implementando BobIA para rodar na mesma porta que web data viz

--- site-institucional/app.js
+++ site-institucional/app.js
@@ -7,6 +7,9 @@ require("dotenv").config({ path: caminho_env });
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
+
+const { GoogleGenAI } = require("@google/genai");
+
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
 
@@ -17,6 +20,31 @@ var usuarioRouter = require("./src/routes/usuarios");
 var unidadesRouter = require("./src/routes/unidades");
 var graficosRouter = require("./src/routes/graficos");
 var jiraRouter = require("./src/routes/jira")
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+async function gerarResposta(mensagem) {
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: `Em um paragráfo responda: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,12 +57,32 @@ app.use(express.static(path.join(__dirname, "public/loginCad")));
 app.use(express.static(path.join(__dirname, "public/Dashboard-estatica")));
 app.use(express.static(path.join(__dirname, "public/simulador-financeiro")));
 
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public/bobia")));
+
 // Rotas
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/unidades", unidadesRouter);
 app.use("/graficos", graficosRouter);
-app.use("/jira", jiraRouter)
+app.use("/jira", jiraRouter);
+
+app.get("/bobia", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/bobia/index.html"));
+});
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
 
 app.listen(PORTA_APP, function () {
     console.log(`
